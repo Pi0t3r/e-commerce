930c89b5de7552ff63a652f8bacbc81589f593b6
Piotr Kapuściński <kapuscinskipiotr5@gmail.com>
2023-01-19 20:42:04 +0100

    Add a react-paginate to change a page for watches

--- e-commerce/src/components/Watches.jsx
+++ e-commerce/src/components/Watches.jsx
@@ -1,40 +1,65 @@
 import { Watches } from "../data/watches";
+import ReactPaginate from "react-paginate";
+import React, { useState } from "react";
 function ShowWatches() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const perPage = 8;
+
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected);
+  };
+
+  const watchesToShow = Watches.slice(
+    currentPage * perPage,
+    (currentPage + 1) * perPage
+  );
+
   return (
-    <div class="w-auto h-96 flex flex-row justify-between flex-wrap mx-20">
-      {Watches.map((watch) => (
-        <div class="flex flex-col w-52 mb-4">
-          <div class="text-right">
-            <p class="cursor-pointer">...</p>
+    <div>
+      <div class="w-auto h-96 flex flex-row justify-between flex-wrap mx-20">
+        {watchesToShow.map((watch) => (
+          <div class="flex flex-col w-52 mb-4">
+            <div class="text-right">
+              <p class="cursor-pointer font-bold text-lg text-slate-500">...</p>
+            </div>
+            <div
+              className="w-full h-44 bg-cover bg-center"
+              style={{ backgroundImage: `url(${watch.image})` }}
+            ></div>
+            <div class="py-4">
+              <p class="font-medium text-lg">{watch.name}</p>
+              <p class="text-slate-500">{watch.Series}</p>
+            </div>
+            <div>
+              <p class="text-slate-500">{watch.ProductCode}</p>
+            </div>
+            <div>
+              <p class="font-medium text-lg">{watch.Price}</p>
+            </div>
+            <div>
+              <p class="font-medium">
+                Box: <span class="text-slate-500 font-normal">{watch.Box}</span>
+              </p>
+              <p class="font-medium">
+                Papers:{" "}
+                <span class="text-slate-500 font-normal">{watch.Papers}</span>
+              </p>
+              <p class="font-medium">
+                Year:{" "}
+                <span class="text-slate-500 font-normal">{watch.Year}</span>
+              </p>
+            </div>
           </div>
-          <div
-            className="w-full h-44 bg-cover bg-center"
-            style={{ backgroundImage: `url(${watch.image})` }}
-          ></div>
-          <div class="py-4">
-            <p class="font-medium text-lg">{watch.name}</p>
-            <p class="text-slate-500">{watch.Series}</p>
-          </div>
-          <div>
-            <p class="text-slate-500">{watch.ProductCode}</p>
-          </div>
-          <div>
-            <p class="font-medium text-lg">{watch.Price}</p>
-          </div>
-          <div>
-            <p class="font-medium">
-              Box: <span class="text-slate-500 font-normal">{watch.Box}</span>
-            </p>
-            <p class="font-medium">
-              Papers:{" "}
-              <span class="text-slate-500 font-normal">{watch.Papers}</span>
-            </p>
-            <p class="font-medium">
-              Year: <span class="text-slate-500 font-normal">{watch.Year}</span>
-            </p>
-          </div>
+        ))}
+        <div class="w-full">
+          <ReactPaginate className="Paginate"
+            previousLabel={"poprzednia"}
+            nextLabel={"następna"}
+            pageCount={2}
+            onPageChange={handlePageClick}
+          />
         </div>
-      ))}
+      </div>
     </div>
   );
 }
