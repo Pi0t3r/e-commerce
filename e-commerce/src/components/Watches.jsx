155e1a4251d605eaf83a857598ed58ae7dc4436b
Piotr Kapuściński <kapuscinskipiotr5@gmail.com>
2023-01-19 17:00:48 +0100

    Added a data about 5 watches, add a components for show watches from data

--- e-commerce/src/components/Watches.jsx
+++ e-commerce/src/components/Watches.jsx
@@ -1,20 +1,42 @@
-import { Watches } from "../data/watches"
+import { Watches } from "../data/watches";
 function ShowWatches() {
   return (
-    {Watches.map((watch ) => (
-      <div>
-            <div>
-                <a>...</a>
-            </div>
-            <div>
-                {watch.image}
-            </div>
-            <div>
-                <p>{watch.name}</p>
-            </div>
+    <div class="w-auto h-96 flex flex-row justify-between flex-wrap mx-20">
+      {Watches.map((watch) => (
+        <div class="flex flex-col w-52 mb-4">
+          <div class="text-right">
+            <p class="cursor-pointer">...</p>
+          </div>
+          <div
+            className="w-full h-44 bg-cover bg-center"
+            style={{ backgroundImage: `url(${watch.image})` }}
+          ></div>
+          <div class="py-4">
+            <p class="font-medium text-lg">{watch.name}</p>
+            <p class="text-slate-500">{watch.Series}</p>
+          </div>
+          <div>
+            <p class="text-slate-500">{watch.ProductCode}</p>
+          </div>
+          <div>
+            <p class="font-medium text-lg">{watch.Price}</p>
+          </div>
+          <div>
+            <p class="font-medium">
+              Box: <span class="text-slate-500 font-normal">{watch.Box}</span>
+            </p>
+            <p class="font-medium">
+              Papers:{" "}
+              <span class="text-slate-500 font-normal">{watch.Papers}</span>
+            </p>
+            <p class="font-medium">
+              Year: <span class="text-slate-500 font-normal">{watch.Year}</span>
+            </p>
+          </div>
         </div>
-    ))}
-  )
+      ))}
+    </div>
+  );
 }
 
 export default ShowWatches;
--- e-commerce/src/data/watches.js
+++ e-commerce/src/data/watches.js
@@ -1,7 +1,8 @@
 export const Watches = [
   {
     id: 1,
-    image: "",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3WNlb1txFDhsK8XX-mvhgts7C9gtZS-oMngJ4yhDe3LyAXwRIoQkzOy6Oh_4NARs46aU&usqp=CAU",
     name: "Vacheron Constantin",
     Series: "Traditionelle",
     Price: "$30,000",
@@ -14,7 +15,8 @@ export const Watches = [
   },
   {
     id: 2,
-    image: "",
+    image:
+      "https://img.alange-soehne.com/open-graph-boxed-image-1/o-dpr-2/9f83e534183f21ecaad9f500b7feaf53b74284c4.jpg",
     name: "A. Lange and Sohne",
     Series: "Richard Lange",
     Price: "$49,995",
@@ -27,9 +29,10 @@ export const Watches = [
   },
   {
     id: 3,
-    image: "",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAdFN_6XBvNbMHZusoDIEcB8Ju6vCLubY76a-7UhjvLlZ5e7fpZQCLK9NQEVWve51TG3c&usqp=CAU",
     name: "IWC",
-    Series: "Pilot's Automatic Top GUn",
+    Series: "Pilot's Automatic Top Gun",
     Price: "$4,715",
     isPromo: false,
     PromoPrice: "",
@@ -40,7 +43,8 @@ export const Watches = [
   },
   {
     id: 4,
-    image: "",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz58kI6gTe1L5MgRN_pf_Gy54vg9NboSUg6g&usqp=CAU",
     name: "Jaeger-LeCoultre",
     Series: "Reverso Duo",
     Price: "$12,500",
@@ -53,7 +57,7 @@ export const Watches = [
   },
   {
     id: 5,
-    image: "",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKUSTVbw1bBOQ-FvnVLOtRHoqD_NwWaBVstw&usqp=CAU",
     name: "Rolex",
     Series: "Daytona",
     Price: "$33,500",
