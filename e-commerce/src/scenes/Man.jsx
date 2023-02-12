1e3dca57954a926a37c5dad9ff675466db87fa13
Piotr Kapuściński <kapuscinskipiotr5@gmail.com>
2023-02-12 15:20:43 +0100

    Add a filter watches after gender in Man.jsx

--- e-commerce/src/scenes/Man.jsx
+++ e-commerce/src/scenes/Man.jsx
@@ -3,6 +3,55 @@ import Sidebar from "../components/Sidebar";
 import { Item } from "./Favourites";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
+import { Watches } from "../data/watches";
+const FilterWatchMan = () => {
+  const filteredWatches = Watches.filter((watch) => watch.gender === "man");
+  return (
+    <div>
+      {filteredWatches.map((watch) => (
+        <div class="mt-2 bg-transparent">
+          <img src={watch.image} class="rounded-xl mx-auto" />
+          <div class="text-white flex flex-row justify-between mx-6 mt-4">
+            <h4 class="text-3xl">${watch.Price}</h4>
+            <ul class="flex flex-row w-1/4 justify-between items-center">
+              <li>
+                <FavoriteIcon />
+              </li>
+              <li>
+                <LocalMallOutlinedIcon />
+              </li>
+            </ul>
+          </div>
+          <div class="text-white mx-6 mt-4 leading-10">
+            <ul>
+              <li>
+                <p>
+                  Name:{" "}
+                  <span class="text-paragraph italic ml-3">{watch.name}</span>
+                </p>
+              </li>
+              <li>
+                <p>
+                  Model:{" "}
+                  <span class="text-paragraph italic ml-3">{watch.Series}</span>
+                </p>
+              </li>
+              <li>
+                <p>
+                  Year:{" "}
+                  <span class="text-paragraph italic ml-3">
+                    Approx. {watch.Year}
+                  </span>
+                </p>
+              </li>
+            </ul>
+          </div>
+          <hr class="mx-6 mt-3" />
+        </div>
+      ))}
+    </div>
+  );
+};
 function Man() {
   return (
     <div>
@@ -16,52 +65,14 @@ function Man() {
         </div>
         <div class="bg-ItemHeader6 bg-cover bg-center w-screen bg-fixed bg-fixed mt-[-40%]">
           <div class="bg-black/70">
-            <div class="pt-52">
-              <Item
-                bgimage="Item"
-                price="2000"
-                icon1={<FavoriteIcon />}
-                icon2={<LocalMallOutlinedIcon />}
-                name="Rolex"
-                model="Day-Date"
-                year="2020"
-              />
-              <Item
-                bgimage="Item"
-                price="2000"
-                icon1={<FavoriteIcon />}
-                icon2={<LocalMallOutlinedIcon />}
-                name="Rolex"
-                model="Day-Date"
-                year="2020"
-              />
-              <Item
-                bgimage="Item"
-                price="2000"
-                icon1={<FavoriteIcon />}
-                icon2={<LocalMallOutlinedIcon />}
-                name="Rolex"
-                model="Day-Date"
-                year="2020"
-              />
-              <Item
-                bgimage="Item"
-                price="2000"
-                icon1={<FavoriteIcon />}
-                icon2={<LocalMallOutlinedIcon />}
-                name="Rolex"
-                model="Day-Date"
-                year="2020"
-              />
-              <Item
-                bgimage="Item"
-                price="2000"
-                icon1={<FavoriteIcon />}
-                icon2={<LocalMallOutlinedIcon />}
-                name="Rolex"
-                model="Day-Date"
-                year="2020"
-              />
+            <div class="pt-40">
+              <p class="text-paragraph px-2 text-center mb-10">
+                Men's watches collection for gentlemen who appreciate elegant
+                style and functionality. Here you will find a wide range of
+                timepieces from top brands, distinguished by durability and
+                precision.
+              </p>
+              <FilterWatchMan />
             </div>
           </div>
         </div>
--- e-commerce/src/scenes/Woman.jsx
+++ e-commerce/src/scenes/Woman.jsx
@@ -2,7 +2,8 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
 import Nav from "../components/Nav";
 import Sidebar from "../components/Sidebar";
-import { Item } from "./Favourites";
+
+
 
 function Woman() {
   return (
@@ -18,51 +19,7 @@ function Woman() {
       <div class="bg-ItemHeader6 bg-cover bg-center w-screen bg-fixed bg-fixed mt-[-40%]">
         <div class="bg-black/70">
           <div class="pt-52">
-            <Item
-              bgimage="Item"
-              price="2000"
-              icon1={<FavoriteIcon />}
-              icon2={<LocalMallOutlinedIcon />}
-              name="Rolex"
-              model="Day-Date"
-              year="2020"
-            />
-            <Item
-              bgimage="Item"
-              price="2000"
-              icon1={<FavoriteIcon />}
-              icon2={<LocalMallOutlinedIcon />}
-              name="Rolex"
-              model="Day-Date"
-              year="2020"
-            />
-            <Item
-              bgimage="Item"
-              price="2000"
-              icon1={<FavoriteIcon />}
-              icon2={<LocalMallOutlinedIcon />}
-              name="Rolex"
-              model="Day-Date"
-              year="2020"
-            />
-            <Item
-              bgimage="Item"
-              price="2000"
-              icon1={<FavoriteIcon />}
-              icon2={<LocalMallOutlinedIcon />}
-              name="Rolex"
-              model="Day-Date"
-              year="2020"
-            />
-            <Item
-              bgimage="Item"
-              price="2000"
-              icon1={<FavoriteIcon />}
-              icon2={<LocalMallOutlinedIcon />}
-              name="Rolex"
-              model="Day-Date"
-              year="2020"
-            />
+            <p></p>
           </div>
         </div>
       </div>
