f8b132fbc661c9e3842bb4b28ad72021cff23c1c
Piotr Kapuściński <kapuscinskipiotr5@gmail.com>
2023-02-23 11:23:44 +0100

    Add a responsive design for Man.jsx

--- e-commerce/src/scenes/Man.jsx
+++ e-commerce/src/scenes/Man.jsx
@@ -27,14 +27,19 @@ const FilterWatchMan = () => {
   return (
     <div>
       {filteredWatches.map((watch) => (
-        <div class="mt-2 bg-transparent" key={watch.id}>
-          <img src={watch.image} class="rounded-xl mx-auto w-9/12" />
-          <div class="text-white flex flex-row justify-between mx-6 mt-4">
-            <h4 class="text-3xl">${watch.Price}</h4>
-            <ul class="flex flex-row w-1/4 justify-between items-center">
+        <div class="mt-2 bg-transparent iPad:relative" key={watch.id}>
+          <img
+            src={watch.image}
+            class="rounded-xl mx-auto w-9/12 iPad:w-[35%] iPad:ml-10 iPad:pb-20"
+          />
+          <div class="text-white flex flex-row justify-between mx-6 mt-4 iPad:absolute iPad:bottom-6 iPad:w-11/12 iPad:flex-row-reverse">
+            <h4 class="text-3xl iPad:mr-20 iPad:text-5xl iPad:font-medium">${watch.Price}</h4>
+            <ul class="flex flex-row w-1/4 justify-between items-center iPad:w-[25%] iPad:pl-20">
               <li
                 onClick={() => toggleFavorite(watch.id)}
-                class={`${isFavorite(watch.id) ? "text-red-600" : "action"} hover:scale-110 transition-all cursor-pointer`}
+                class={`${
+                  isFavorite(watch.id) ? "text-red-600" : "action"
+                } hover:scale-110 transition-all cursor-pointer`}
               >
                 <FavoriteIcon />
               </li>
@@ -43,7 +48,7 @@ const FilterWatchMan = () => {
               </li>
             </ul>
           </div>
-          <div class="text-white mx-6 mt-4 leading-10">
+          <div class="text-white mx-6 mt-4 leading-10 iPad:absolute iPad:right-0 iPad:top-0 iPad:text-xl iPad:w-1/2">
             <ul>
               <li>
                 <p>
@@ -71,15 +76,15 @@ function Man() {
       <Nav />
       <Sidebar />
       <div>
-        <div class="w-full mt-20">
-          <h3 class="text-paragraph uppercase italic text-center text-3xl">
+        <div class="w-full mt-40 iPad:mt-24">
+          <h3 class="text-paragraph uppercase italic text-center text-3xl iPad:pb-40 iPad:text-5xl">
             Man
           </h3>
         </div>
         <div class="bg-ItemHeader6 bg-cover bg-center w-screen bg-fixed mt-[-40%]">
           <div class="bg-black/70">
             <div class="pt-40">
-              <p class="text-paragraph px-2 text-center mb-10">
+              <p class="text-paragraph px-2 iPad:px-8 text-center mb-10 iPad:mt-8">
                 Men's watches collection for gentlemen who appreciate elegant
                 style and functionality. Here you will find a wide range of
                 timepieces from top brands, distinguished by durability and
