022e746f9e3154c1979749ae14be92deddf31695
Piotr Kapuściński <kapuscinskipiotr5@gmail.com>
2023-02-23 16:51:09 +0100

    Add a responsive design for Basket.jsx

--- e-commerce/src/components/ItemCart.jsx
+++ e-commerce/src/components/ItemCart.jsx
@@ -6,9 +6,9 @@ const Button = ({ text }) => {
 
 function ItemCart({ name, money, code }) {
   return (
-    <div class="w-full h-40 flex flex-row px-2">
+    <div class="w-full h-40 flex flex-row px-2 my-4">
       <div class="w-1/2 h-full flex items-center justify-center">
-        <div class="bg-Item bg-center bg-cover w-[90%] h-[90%] rounded-xl drop-shadow-[4px_10px_5px_rgba(0,0,0,0.25)]" />
+        <div class="bg-Item bg-center bg-cover w-[90%] h-[90%] rounded-xl drop-shadow-[4px_10px_5px_rgba(0,0,0,0.25)] tablet:w-3/4 tablet:h-full" />
       </div>
       <div class="flex flex-col w-1/2">
         <div class="flex flex-row justify-between w-full text-paragraph mt-3">
@@ -17,10 +17,10 @@ function ItemCart({ name, money, code }) {
             <DeleteForeverOutlinedIcon />
           </button>
         </div>
-        <div class="font-medium text-xl mt-4">
+        <div class="font-medium text-xl mt-4 text-Main-text">
           <h3>{name}</h3>
         </div>
-        <div class="flex flex-row justify-between mt-6 items-center">
+        <div class="flex flex-row justify-between mt-6 items-center text-Main-text">
           <p class="font-medium text-xl">${money}</p>
           <div class="flex flex-row mr-5 w-[40%] justify-between items-center">
             <Button text="-" />
